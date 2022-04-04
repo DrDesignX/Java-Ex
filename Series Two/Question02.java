01a8e686d68a79e2e82a5f9c2f4e3bc633117469
MohamadAmin Seydi <81829038+DrDesignX@users.noreply.github.com>
2022-04-04 11:57:27 +0430

    Add Question 02

---
+++ Series Two/Question02.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+class Question02 {
+    /* 
+        Github  : https://github.com/DrDesignX/Java-Ex
+        Authors : MohamadAmin Seydi & MohamadReza Barkhordari 
+    */
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Please enter First String :");
+        String first = inp.nextLine();
+        System.out.println("Please enter First String :");
+        String second = inp.nextLine();
+        // System.out.println(stringsEqual(first, second));
+        // System.out.println(containsNumber(second));
+        // System.out.println(lengthGreaterThanEight(first));
+        // System.out.println(containsSpecialChar(first));
+        System.out.println(stringComparison(first, second));
+    }
+
+    public static boolean stringsEqual(String first, String second) {
+        return first.equals(second);
+    }
+
+    public static boolean containsNumber(String string) {
+        int digitCount = 0;
+        char[] stringChars = stringToChar(string);
+        for (char ch : stringChars) {
+            if (Character.isDigit(ch)) {
+                digitCount++;
+            }
+        }
+        if (digitCount > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean lengthGreaterThanEight(String string) {
+        return string.length() > 8;
+    }
+
+    public static boolean containsSpecialChar(String string) {
+        int count = 0;
+
+        String s = string;
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isDigit(s.charAt(i)) && !Character.isLetter(s.charAt(i)) && !Character.isWhitespace(s.charAt(i))) {
+                count++;
+            }
+        }
+        if (count == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static boolean stringComparison(String first, String second) {
+        return stringsEqual(first, second) &&  containsNumber(first) && lengthGreaterThanEight(first) && containsSpecialChar(first);
+    }
+
+    public static char[] stringToChar(String str) {
+        char[] ch = new char[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            ch[i] = str.charAt(i);
+        }
+        return ch;
+    }
+
+}
