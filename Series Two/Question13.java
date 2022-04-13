5e5b7a7de7e393fc7548ae366ff4ab1bb9842f54
MohamadAmin Seydi <81829038+DrDesignX@users.noreply.github.com>
2022-04-13 23:59:47 +0430

    Update Question13.java

--- Series Two/Question13.java
+++ Series Two/Question13.java
@@ -10,16 +10,16 @@ class Question13 {
             int n = inp2.nextInt();
             System.out.println("The result is: " + RecAckerman(m, n));
         }
-
     }
     public static int RecAckerman(int m, int n) {
         if (m == 0) {
             return n + 1;
-        } else if ((m > 0) && (n == 0)) {
+        } else if (n == 0) {
             return RecAckerman(m - 1, 1);
-        } else if ((m > 0) && (n > 0)) {
+        } else {
             return RecAckerman(m - 1, RecAckerman(m, n - 1));
-        } else
-            return n + 1;
+            // int temp = RecAckerman(m, n - 1);
+            // return RecAckerman(m - 1, temp);
+        }
     }
 }
